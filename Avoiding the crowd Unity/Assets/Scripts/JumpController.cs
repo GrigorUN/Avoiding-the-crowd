edb14c8c7da3de679a3ff188323b3567f8808eb9
GrigorUN <grigorogannisyan.12@gmail.com>
2023-02-19 16:49:55 +0300

    Скучно было на работе

---
+++ Avoiding the crowd Unity/Assets/Scripts/JumpController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpController : MonoBehaviour
+{
+    public float jumpForce = 10f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+}
