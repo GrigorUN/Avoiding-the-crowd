036e2ed187ce99c57fd8389faf9cee838ed134e3
GrigorUN <grigorogannisyan.12@gmail.com>
2023-02-23 12:12:41 +0300

    Обновление

--- Avoiding the crowd Unity/Assets/Scripts/JumpController.cs
+++ Avoiding the crowd Unity/Assets/Scripts/JumpController.cs
@@ -6,6 +6,7 @@ public class JumpController : MonoBehaviour
 {
     public float jumpForce = 10f;
     private Rigidbody2D rb;
+    private bool hasJumped = false;
 
     void Start()
     {
@@ -14,9 +15,21 @@ public class JumpController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            if (!hasJumped)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                hasJumped = true;
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            hasJumped = false;
         }
     }
 }
