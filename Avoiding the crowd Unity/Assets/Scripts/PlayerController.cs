f1ee8f86c4573dd51cbca71185dab5bfce8a9408
GrigorUN <grigorogannisyan.12@gmail.com>
2023-02-28 09:41:04 +0300

    fixbags

--- Avoiding the crowd Unity/Assets/Scripts/PlayerController.cs
+++ Avoiding the crowd Unity/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -69,5 +69,87 @@ public class PlayerController : MonoBehaviour
     {
         yield return new WaitForSeconds(0.5f); // ждем 1 секунду перед перезапуском игры
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // перезапускаем текущий уровень
+    }
+}
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 10f;
+    private Rigidbody2D rb;
+    private bool isGameOver = false;
+    private bool hasJumped = false;
+    private bool isOnPlatform = false; // флаг, указывающий, находитс€ ли игрок на платформе
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (!isGameOver)
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+    }
+
+    void Update()
+    {
+        // провер€ем, можно ли прыгнуть
+        if ((Input.GetKeyDown(KeyCode.Space) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)) && isOnPlatform && !hasJumped && !isGameOver)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            hasJumped = true;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            hasJumped = false;
+            isOnPlatform = true; // игрок находитс€ на платформе
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("¬ы проиграли!");
+            isGameOver = true;
+            RestartGame();
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            isOnPlatform = false; // игрок больше не находитс€ на платформе
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Fall"))
+        {
+            Debug.Log("¬ы проиграли!");
+            isGameOver = true;
+            RestartGame();
+        }
+    }
+
+    void RestartGame()
+    {
+        StartCoroutine(RestartDelay());
+    }
+
+    IEnumerator RestartDelay()
+    {
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
